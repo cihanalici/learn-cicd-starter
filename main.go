6f2626a24a537f8519bc7e4f87d22fc882f708cc
cihanalici <cihan_cihan27@hotmail.com>
2024-05-22 14:16:57 +0300

    ~gcloud

--- main.go
+++ main.go
@@ -42,6 +42,7 @@ func main() {
 	// https://github.com/libsql/libsql-client-go/#open-a-connection-to-sqld
 	// libsql://[your-database].turso.io?authToken=[your-auth-token]
 	dbURL := os.Getenv("DATABASE_URL")
+
 	if dbURL == "" {
 		log.Println("DATABASE_URL environment variable is not set")
 		log.Println("Running without CRUD endpoints")
